e9b0aef0e1d033e9c35f6697b028fb15b60625ff
Lucas Rodrigues <lukinhasr1423@gmail.com>
2022-12-16 15:57:50 -0300

    criado e renderizado os cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,10 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
 export default function Inicio() {
   return (
@@ -11,6 +14,11 @@ export default function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
       <Rodape />
     </>
   )
