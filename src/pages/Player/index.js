70633d80f1e72d45ca6d3b73c08c0417e66aec0c
Lucas Rodrigues <lukinhasr1423@gmail.com>
2022-12-17 11:48:53 -0300

    finalizado a aula 4

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,6 +3,7 @@ import Titulo from 'components/Titulo'
 import { useParams } from 'react-router-dom'
 import videos from 'json/db.json'
 import styles from './Player.module.css'
+import NaoEncontrada from 'pages/NaoEncontrada'
 
 export default function Player() {
   const parametros = useParams()
@@ -10,7 +11,10 @@ export default function Player() {
     return video.id === Number(parametros.id)
   })
 
-  console.log(video);
+  if (!video) {
+    return <NaoEncontrada />
+  }
+
   return (
     <>
       <Banner imagem="player" />
